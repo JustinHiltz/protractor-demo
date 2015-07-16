6ebff17c21d17517ab7bdb4761006709d1f7e7b7
Justin Hiltz <justin4770@gmail.com>
2015-07-15 20:19:40 -0400

    Finished the tutorial

--- conf.js
+++ conf.js
@@ -1,5 +1,10 @@
 // conf.js
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['spec.js']
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
+  multiCapabilities: [{
+    browserName: 'safari'
+  }, {
+    browserName: 'chrome'
+  }]
 }
